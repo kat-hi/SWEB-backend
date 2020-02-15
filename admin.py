465ee2182de9c10fd38660bc4917ad165b1ae133
kat-hi <s77518@beuth-hochschule.de>
2020-02-15 17:22:49 +0700

    add authentication to admin backend

--- admin.py
+++ admin.py
@@ -1,23 +1,36 @@
 from flask_admin.contrib.sqla import ModelView
 import main
+from flask_login import current_user
 
-class pflanzlistetable(ModelView):
+
+class AuthenticatedView(ModelView):
+	def is_accessible(self):
+		return current_user.is_authenticated
+
+	def inaccessible_callback(self, name, **kwargs):
+		# redirect to login page if user doesn't have access
+		return 'sorry. your not authorized'
+
+
+class pflanzlistetable(AuthenticatedView):
 	main.app.app_context().push()
+	can_create = False
 	can_delete = False
 	can_edit = False
 	can_export = True
 	can_set_page_size = 532
 
 
-class obstsortentable(ModelView):
+class obstsortentable(AuthenticatedView):
 	main.app.app_context().push()
+	can_create = False
 	can_delete = False
 	can_edit = False
 	can_export = True
 	can_set_page_size = 532
 
 
-class patentable(ModelView):
+class patentable(AuthenticatedView):
 	main.app.app_context().push()
 	can_edit = False
 	can_export = True
