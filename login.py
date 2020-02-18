211b75951309bec1fb19ed5407d0b179972b3dcd
kat-hi <s77518@beuth-hochschule.de>
2020-02-18 22:18:59 +0700

    adapt new config structure and add second admin email

--- login.py
+++ login.py
@@ -13,7 +13,8 @@ users_email = ""
 # TODO HttpError handling
 # getting the provider configuration document
 def get_google_provider_cfg():
-	return requests.get(Config.GOOGLE_DISCOVERY_URL).json()
+	return requests.get(Config.LOGIN['GOOGLE_DISCOVERY_URL']).json()
+
 
 
 @login_manager.user_loader
@@ -43,7 +44,7 @@ def load_user(id):
 def flask_user_authentication(users_email):
 	from models import Admin
 	from main import DB, app
-	if users_email == Config.ADMIN_EMAIL:
+	if users_email == Config.LOGIN['ADMIN_EMAIL_1'] or users_email == Config.LOGIN['ADMIN_EMAIL_2']:
 		app.logger.info('FLASK USER AUTH; USER_EMAIL EQUALS ADMIN_EMAIL')
 		admin = DB.session.query(Admin).get(users_email)
 		admin.authenticated = "true"
@@ -77,7 +78,7 @@ def google_login():
 	authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 	app.logger.info('got auth-endpoint: ' + authorization_endpoint)
 	# Use library to construct request for Google login + provide scopes that let retrieve user's profile from Google
-	request_uri = Config.CLIENT.prepare_request_uri(
+	request_uri = Config.LOGIN['CLIENT'].prepare_request_uri(
 		authorization_endpoint,
 		redirect_uri=request.base_url.replace('http://', 'https://') + "/callback",
 		scope=["openid", "email", "profile"])
@@ -95,7 +96,7 @@ def callback():
 	google_provider_cfg = get_google_provider_cfg()
 	token_endpoint = google_provider_cfg["token_endpoint"]
 	app.logger.info('GOT TOKEN_ENDPOINT from /callback ' + token_endpoint)
-	token_url, headers, body = Config.CLIENT.prepare_token_request(
+	token_url, headers, body = Config.LOGIN['CLIENT'].prepare_token_request(
 		token_endpoint,
 		authorization_response=request.url.replace('http://', 'https://'),
 		redirect_url=request.base_url.replace('http://', 'https://'),
@@ -108,11 +109,11 @@ def callback():
 		auth=(Config.SECRETS['GOOGLE_CLIENT_ID'], Config.SECRETS['GOOGLE_CLIENT_SECRET']))
 
 	app.logger.info('GOT TOKEN RESPONSE from /callback ' + str(token_response))
-	Config.CLIENT.parse_request_body_response(json.dumps(token_response.json()))
+	Config.LOGIN['CLIENT'].parse_request_body_response(json.dumps(token_response.json()))
 	# find and hit the URL from Google that gives you the user's profile information,
 	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
 	app.logger.info('GOT USERINFO_ENDPOINT from /callback ' + userinfo_endpoint)
-	uri, headers, body = Config.CLIENT.add_token(userinfo_endpoint)
+	uri, headers, body = Config.LOGIN['CLIENT'].add_token(userinfo_endpoint)
 	userinfo_response = requests.get(uri, headers=headers, data=body)
 	app.logger.info('GOT USERINFO_RESPONSE from /callback ' + str(userinfo_response))
 
@@ -123,7 +124,7 @@ def callback():
 	users_name = userinfo_response.json()["given_name"]
 	app.logger.info('GOT USER DATA from /callback: ' + unique_id + ' ' + users_email + ' ' + users_name)
 	app.logger.info('CURRENT USER: ' + str(current_user))
-	app.logger.info('ADMIN EMAIL: ' + str(Config.ADMIN_EMAIL) + ' and user mail: ' + str(users_email))
+	app.logger.info('ADMIN EMAIL: ' + str(Config.LOGIN['ADMIN_EMAIL_1']) + ' ' + str(Config.LOGIN['ADMIN_EMAIL_2']) + ' and user mail: ' + str(users_email))
 
 	if flask_user_authentication(users_email):
 		return redirect('https://demo.datexis.com/admin')
