a653b03ce63d52d5109e9be174838b2e8ccf42cc
kat-hi <s77518@beuth-hochschule.de>
2020-02-18 22:18:29 +0700

    more structure

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 from oauthlib.oauth2 import WebApplicationClient
 
-# .env provides all env-variables that are used by production or development mode
 class Config():
 	DATABASE = {
 		'HOST': os.environ['HOST'],
 		'USER': os.environ['USER'],
-		'PASSWORD': os.environ['PASSWORD'],
+		'PASSWORD': os.environ['DB_PASSWORD'],
 		'DBNAME': os.environ['DATABASE'],
 	}
 
@@ -14,12 +13,23 @@ class Config():
 		'GOOGLE_CLIENT_ID': os.environ.get('GOOGLE_CLIENT_ID', None),
 		'GOOGLE_CLIENT_SECRET': os.environ.get('GOOGLE_CLIENT_SECRET', None),
 		'SECRET_KEY': os.environ.get("SECRET_KEY"),
-		'GOOGLE_API_KEY': os.environ.get('GOOGLE_API_KEY')
 	}
 
-	ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
-	CLIENT = WebApplicationClient(SECRETS['GOOGLE_CLIENT_ID'])
-	GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+	LOGIN = {
+		'ADMIN_EMAIL_1': os.environ['ADMIN_EMAIL_1'],
+		'ADMIN_EMAIL_2': os.environ['ADMIN_EMAIL_2'],
+		'CLIENT': WebApplicationClient(SECRETS['GOOGLE_CLIENT_ID']),
+		'GOOGLE_DISCOVERY_URL': 'https://accounts.google.com/.well-known/openid-configuration'
+	}
+
+	SMTP = {
+		'PORT': os.environ['SMTP_PORT'],
+		'SERVER': os.environ['SMTP_SERVER'],
+		'SENDER': os.environ['SENDER_EMAIL'],
+		'RECEIVER': os.environ['RECEIVER_EMAIL'],
+		'PASSWORD': os.environ['SMTP_PASSWORD']
+	}
+
 
 	from main import app
 	app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -27,7 +37,7 @@ class Config():
 	app.config['SQLALCHEMY_ECHO'] = True  # debugging purpose
 	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + DATABASE['USER'] + ':' + DATABASE['PASSWORD'] + '@' \
 	                                        + DATABASE['HOST'] + '/' + DATABASE['DBNAME']
-
+	
 
 class Production(Config):
 	DATABASE = {
