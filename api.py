6ec4b6239382198502c5da35ae0e23d7e409c34d
kat-hi <s77518@beuth-hochschule.de>
2020-03-05 16:30:04 +0100

    change cross origin policy

--- api.py
+++ api.py
@@ -1,22 +1,23 @@
 import simplejson
 from flask import request, Blueprint
 from flask_cors import cross_origin
-from flask import jsonify
+from flask import jsonify, redirect
 import json
 
 api = Blueprint('api', __name__)
 
+
 @api.route('/api', methods=['GET'])
-@cross_origin(origin='localhost:3000/')
+@cross_origin()
 def index():
 	response = jsonify({'json sagt': 'Hallo i bims. der json.'})
 	return response
 
 
 @api.route('/api/karte', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte')
+@cross_origin()
 def infos():
-	from main import DB
+	from main import DB, app
 	import models, schemas
 
 	tree_results = DB.session.query(models.Pflanzliste).all()
@@ -28,15 +29,15 @@ def infos():
 	paten_output = paten_schema.dump(paten_results)
 
 	for tree in trees_output:
-			if tree["id"] in paten_output[0].values():
+		for pate in paten_output:
+			app.logger.info('BAUMID: ' + str(tree['baumID']) + ' and PATENVALUES: ' + str(pate.values()))
+			if str(tree["baumID"]) in pate.values():
+				app.logger.info('BAUM IN PATE: ' + str(tree['baumID']))
 				tree["pate"] = "true"
-			else:
-				tree["pate"] = "false"
 	return simplejson.dumps(trees_output, ensure_ascii=False, encoding='utf8')
 
-
 @api.route('/api/karte/baeume', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte/baeume')
+@cross_origin()
 def get_trees():
 	from main import DB
 	import models, schemas
@@ -47,7 +48,7 @@ def get_trees():
 
 
 @api.route('/api/karte/baeume/<id>', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte/baeume/id')
+@cross_origin()
 def get_tree(id):
 	import schemas, models
 	from main import DB
@@ -58,7 +59,7 @@ def get_tree(id):
 
 
 @api.route('/api/karte/baeume/koordinaten', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte/baeume/koordinaten')
+@cross_origin()
 def get_coordinates():
 	import schemas, models
 	from main import DB
@@ -69,7 +70,7 @@ def get_coordinates():
 
 
 @api.route('/api/karte/baeume/<id>/koordinaten', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte/baeume/<id>/koordinaten')
+@cross_origin()
 def get_coordinates_of_tree(id):
 	import schemas, models
 	from main import DB
@@ -80,7 +81,7 @@ def get_coordinates_of_tree(id):
 
 
 @api.route('/api/karte/baeume/properties', methods=['GET'])
-@cross_origin(origin='localhost:3000/karte/baeume/properties')
+@cross_origin()
 def get_properties():
 	import schemas, models
 	from main import DB
@@ -92,6 +93,7 @@ def get_properties():
 
 # TODO
 @api.route('/api/kontakt', methods=['POST'])
+@cross_origin()
 def fetch_contact_information():
 	from mail import log_into_SMTP_Server_and_send_email
 	response = json.loads(request.data.decode('utf-8'))
