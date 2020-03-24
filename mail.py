8722fafdcafdf40897593cb2e927ece13a3061f1
kat-hi <s77518@beuth-hochschule.de>
2020-03-25 00:59:43 +0100

    fixed phone issue and new mailformat

--- mail.py
+++ mail.py
@@ -9,19 +9,24 @@ SERVER = Config.SMTP['SERVER']
 SENDER = Config.SMTP['SENDER']
 RECEIVER = Config.SMTP['RECEIVER']
 PASSWORD = Config.SMTP['PASSWORD']
+from main import app
 
-def plain_text_mail(firstname, lastname, email, address, user_message):
-	return 'Absender\n: ' + firstname + ' ' + lastname + '\n' + email + '\n' + address + '\n\n' + 'Nachricht:\n' + user_message
+def plain_text_mail(firstname, lastname, email, phone, streetaddress, cityaddress, user_message):
+	app.logger.info('PLAINTEXT: ' + firstname+ ' ' +lastname+ ' ' + email+ ' ' +str(phone) + ' ' + streetaddress + ' ' + cityaddress+ ' ' + user_message)
+	return 'Absender:\n' + firstname + ' ' + lastname + '\n' + streetaddress + '\n' + cityaddress + '\n' + email + '\n' + 'Tel: ' + str(phone) + '\n\n' + 'Nachricht:\n' + user_message
 
 
-def log_into_SMTP_Server_and_send_email(firstname, lastname, email, address, user_message):
+
+def log_into_SMTP_Server_and_send_email(firstname, lastname, email, phone, streetaddress, cityaddress, user_message):
+	app.logger.info('LOGINTO: ' + firstname + ' ' + lastname + ' ' + email + ' ' + str(phone)+ ' ' + streetaddress + ' ' +cityaddress+ ' ' +user_message)
+
 	message = MIMEMultipart("alternative")
-	message["Subject"] = "multipart test"
-	message["From"] = SENDER
+	message["Subject"] = "Anfrage: Baumpatenschaft"
+	message["From"] = RECEIVER
 	message["To"] = RECEIVER
-
-	part1 = MIMEText(plain_text_mail(firstname, lastname, email, address, user_message), "plain")
+	part1 = MIMEText(plain_text_mail(firstname, lastname, email, phone, cityaddress, streetaddress, user_message), "plain")
 	message.attach(part1)
+	app.logger.info(str(part1))
 	context = ssl.create_default_context()
 
 	try:
