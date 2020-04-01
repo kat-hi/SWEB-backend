d2dcb2640527b8dfe79d10bcc44968e34c2d29d3
kat-hi <s77518@beuth-hochschule.de>
2020-04-01 19:13:45 +0200

    change response status

--- sweb_backend/api.py
+++ sweb_backend/api.py
@@ -9,16 +9,12 @@ import re
 api = Blueprint('api', __name__)
 from main import app
 
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-	default_limits=['50 per day']
-)
+limiter = Limiter(app, key_func=get_remote_address, default_limits=['200 per day'])
 
 @api.route('/api', methods=['GET'])
 def index():
 	response = jsonify({'json sagt': 'Hallo i bims. der json.'})
-	return response
+	return response, 200
 
 
 @api.route('/api/karte', methods=['GET'])
@@ -26,12 +22,10 @@ def index():
 def infos():
 	from main import DB
 	import models, schemas
-
 	tree_results = DB.session.query(models.Pflanzliste).all()
 	tree_schema = schemas.Tree(many=True)
 	trees_output = tree_schema.dump(tree_results)
-
-	return simplejson.dumps(trees_output, ensure_ascii=False, encoding='utf8')
+	return simplejson.dumps(trees_output, ensure_ascii=False, encoding='utf8'), 200
 
 
 @api.route('/api/karte/baeume', methods=['GET'])
@@ -53,7 +47,7 @@ def get_tree(id):
 	tree_results = DB.session.query(models.Pflanzliste).get(id)
 	tree_schema = schemas.Tree()
 	tree_output = tree_schema.dump(tree_results)
-	return simplejson.dumps(tree_output, ensure_ascii=False, encoding='utf8')
+	return simplejson.dumps(tree_output, ensure_ascii=False, encoding='utf8'), 200
 
 
 @api.route('/api/karte/baeume/koordinaten', methods=['GET'])
@@ -64,7 +58,7 @@ def get_coordinates():
 	tree_results = DB.session.query(models.Pflanzliste).all()
 	schema = schemas.Treecoordinates(many=True)
 	output = schema.dump(tree_results)
-	return simplejson.dumps(output, ensure_ascii=False, encoding='utf8')
+	return simplejson.dumps(output, ensure_ascii=False, encoding='utf8'), 200
 
 
 @api.route('/api/karte/baeume/<id>/koordinaten', methods=['GET'])
@@ -75,7 +69,7 @@ def get_coordinates_of_tree(id):
 	tree_results = DB.session.query(models.Pflanzliste).get(id)
 	schema = schemas.Treecoordinates()
 	output = schema.dump(tree_results)
-	return simplejson.dumps(output, ensure_ascii=False, encoding='utf8')
+	return simplejson.dumps(output, ensure_ascii=False, encoding='utf8'), 200
 
 
 @api.route('/api/karte/baeume/properties', methods=['GET'])
@@ -98,10 +92,11 @@ def get_imagelinks():
 		if response.status_code is 200 and (response.headers['Content-Type'] == 'image/png' or response.headers['Content-Type'] == 'image/jpeg'):
 			app.logger.info(str(response))
 			checked_files.append(base_download_url+image_id)
-	return jsonify({'data': checked_files})
+	return jsonify({'data': checked_files}), 200
 
 
 @api.route('/api/kontakt', methods=['POST'])
+@limiter.limit('10 per hour', override_defaults=False)
 def fetch_contact_information():
 	from mail import log_into_SMTP_Server_and_send_email
 	response = json.loads(request.data.decode('utf-8'))
@@ -112,6 +107,6 @@ def fetch_contact_information():
 	message = str(response['message'])
 	firstname = str(response['firstName'])
 	phone = str(response['phone'])
-	app.logger.info('API INFO: ' + email + ' ' + lastname+ ' ' + firstname + ' ' + cityaddress + ' ' + streetaddress+ ' ' + message + ' ' + phone)
+	app.logger.info('API INFO: ' + email + ' ' + lastname + ' ' + firstname + ' ' + cityaddress + ' ' + streetaddress + ' ' + message + ' ' + phone)
 	log_into_SMTP_Server_and_send_email(firstname, lastname, email, phone, streetaddress, cityaddress, message)
 	return '', 200
