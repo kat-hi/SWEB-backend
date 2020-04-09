a5e22fcedec2bf7b5bd6332b2596f4eb60a010c0
kat-hi <s77518@beuth-hochschule.de>
2020-04-09 12:55:14 +0200

    refactor: adapt config reference

--- sweb_backend/main.py
+++ sweb_backend/main.py
@@ -10,44 +10,44 @@ from flask_limiter import Limiter
 
 DB = SQLAlchemy()
 MA = Marshmallow()
-admin = Admin(template_mode='bootstrap3')
+AD = Admin(template_mode='bootstrap3')
 login_manager = LoginManager()
+logging.basicConfig(level=logging.DEBUG)
 
 def create_app():
-	app = Flask('__main__')
-	DB.init_app(app)  # initialize SQLAlchemy
-	MA.init_app(app)  # initialize Marshmallow
-	admin.init_app(app)
+	app = Flask(__name__)
+	DB.init_app(app)
+	MA.init_app(app)
+	AD.init_app(app)
 	login_manager.init_app(app)
 	return app
 
-
 def set_environment():
-	app.app_context().push()
-	from sweb_backend.config import Config
+	app.config['FLASK_ADMIN_SWATCH'] = 'spacelab'
 	if os.environ['FLASK_ENV'] == 'dev':
 		app.config.from_object('sweb_backend.config.Config')
-		print(app.config['SQLALCHEMY_DATABASE_URI'])
 	else:
 		app.config.from_object('sweb_backend.config.Production')
-		print(app.config['SQLALCHEMY_DATABASE_URI'])
-	app.secret_key = Config.SECRETS['SECRET_KEY']
 
 
 def create_tables():
 	app.app_context().push()
 	from sweb_backend import models
-	from sweb_backend.admin import pflanzlistetable, obstsortentable, imagetable
-	admin.add_view(imagetable(models.Image, DB.session))
-	admin.add_view(pflanzlistetable(models.Pflanzliste, DB.session))
-	admin.add_view(obstsortentable(models.Sorten, DB.session))
+	from sweb_backend.admin_views import pflanzlistetable, obstsortentable, imagetable
+	AD.add_view(imagetable(models.Image, DB.session))
+	AD.add_view(pflanzlistetable(models.Plantlist, DB.session))
+	AD.add_view(obstsortentable(models.Sorts, DB.session))
+
+
+def register_all_blueprints():
+	app.app_context().push()
+	from sweb_backend.login import admin_login
+	from sweb_backend.api import api
+	app.register_blueprint(admin_login)
+	app.register_blueprint(api)
 
 
 app = create_app()
-logging.basicConfig(level=logging.DEBUG)
-
-
-
 limiter = Limiter(
 	app,
 	key_func=get_remote_address,
@@ -55,13 +55,6 @@ limiter = Limiter(
 )
 
 set_environment()
-
 create_tables()
-
-from sweb_backend.login import admin_login
-from sweb_backend.api import api
-
-app.register_blueprint(admin_login)
-app.register_blueprint(api)
-
+register_all_blueprints()
 
